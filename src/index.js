f91950fd5e7f15435327822153be70ea9854d47b
Himanshu <hp2146@gmail.com>
2021-08-12 11:04:14 +0530

    test

--- src/index.js
+++ src/index.js
@@ -30,7 +30,7 @@ app.get("/get-auth-code", (req, res, next) => {
   );
 });
 app.get("/redirect",async (req,res)=>{
-  const pathname = 'https://miiotest.herokuapp.com/redirect?code=AQDNyYS0CWR4cbygXjewFH3R4QZZ_EIWQ023pQGAwNfZCsNywVz8jJsOgJxLave5y9RuJSL10rGVC5MRpirZN9X_oxdBH33wGm9mQh0U190dEg8bVpXMEJPp2WonHVYM6WxlAVy_cyZToUEGgtg6YubaTlsyZhCI5CkB5IXMaq_EZwNv2kdg06v2IijP_sef6llEHcS13woWfBpWkL884ZT2gjonsQWA4_tzbg-6G-sOog#_'
+  const pathname = req.query.code
   let code;
   code = pathname.substring(0, pathname.length - 2);
   console.log("Himanshu",code)
